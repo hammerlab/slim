3d55277d81c9180c6e9659228fdabb4bd3d31b61
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-20 21:50:57 +0000

    various package.json fixes

--- slim.js
+++ slim.js
@@ -6,6 +6,10 @@ var mongoHost = argv.h || argv['mongo-host'] || 'localhost';
 var mongoDb = argv.d || argv['mongo-db'] || 'meteor';
 var mongoUrl = argv.m || argv['mongo-url'] || ('mongodb://' + mongoHost + ':' + mongoPort + '/' + mongoDb);
 
+if (!mongoUrl.match(/^mongodb:\/\//)) {
+  mongoUrl = 'mongodb://' + mongoUrl;
+}
+
 var Server = require('./server').Server;
 new Server(mongoUrl);
 
