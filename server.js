f85059ee5126ec4a85b8635585a85e30f5455a59
Ryan Williams <ryan.blake.williams@gmail.com>
2015-09-22 21:39:27 +0000

    log an error if empty MetricsUpdate event recv'd

--- server.js
+++ server.js
@@ -738,7 +738,11 @@ var handlers = {
   },
   SparkListenerExecutorMetricsUpdate: function(app, e) {
     var executor = app.getExecutor(e);
-    l.debug("processing %d metrics updates..", e['Metrics Updated'].length);
+    if (!e['Metrics Updated']) {
+      // Depend on JsonRelay to filter out empty MetricsUpdate events.
+      l.error("Got SparkListenerExecutorMetricsUpdate event with empty 'Metrics Updated': ", JSON.stringify(e));
+      return;
+    }
     e['Metrics Updated'].map(function(m) {
       var stage = app.getStage(m);
       var job = app.getJob(stage.get('jobId'));
